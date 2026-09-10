6b804bd7477a713d2049dbb7f01168812b50e052
Vansh <techsfc120.zz@gmail.com>
2026-09-10 23:41:23 +0530

    adding quit feature

--- player.js
+++ player.js
@@ -1,4 +1,5 @@
 import audio from 'audio';
+import readline from 'node:readline';
 
 const file = process.argv[2];
 
@@ -7,14 +8,25 @@ if (!file) {
   process.exit(1);
 }
 
-// Load audio file
 const track = await audio(file);
 
-// Play audio
 track.play();
 console.log(`Playing: ${file}`);
+console.log("Press 'q' to quit");
 
-// Exit when finished
+// Enable keypress events on stdin
+readline.emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+}
+
+// Listen for keys
+process.stdin.on('keypress', (str, key) => {
+  if (str === 'q' || (key && key.ctrl && key.name === 'c')) {
+    process.exit(0);
+  }
+});
+
 track.on('ended', () => {
   process.exit(0);
 });
